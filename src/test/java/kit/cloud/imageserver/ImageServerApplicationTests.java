da118b40b4f358e7d8209aa5bfeb4c6edda02dce
smj1513 <minj21@naver.com>
2025-05-31 03:03:42 +0900

    git init

---
+++ src/test/java/kit/cloud/imageserver/ImageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kit.cloud.imageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
