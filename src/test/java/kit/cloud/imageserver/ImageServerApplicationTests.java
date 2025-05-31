a8942756f9ccd0402301fb74a7612ed6183eec90
Kakao1513 <42140061+Kakao1513@users.noreply.github.com>
2025-05-31 22:43:26 +0900

    [add] docker compose, github action init

--- src/test/java/kit/cloud/imageserver/ImageServerApplicationTests.java
+++ src/test/java/kit/cloud/imageserver/ImageServerApplicationTests.java
@@ -3,10 +3,8 @@ package kit.cloud.imageserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ImageServerApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
